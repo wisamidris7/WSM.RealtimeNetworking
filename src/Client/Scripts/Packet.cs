1eaf668fa6ccb1b350a78606652c10714833a52d
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-04-02 21:45:32 +0300

    Update Packet.cs

--- src/Client/Scripts/Packet.cs
+++ src/Client/Scripts/Packet.cs
@@ -22,7 +22,7 @@ public class Packet : IDisposable
     /// <summary>Creates a new empty packet (without an ID).</summary>
     public Packet()
     {
-        buffer = []; // Initialize buffer
+        buffer = new List<byte>(); // Initialize buffer
         readPos = 0; // Set readPos to 0
     }
 
@@ -30,7 +30,7 @@ public class Packet : IDisposable
     /// <param name="_id">The packet ID.</param>
     public Packet(int _id)
     {
-        buffer = []; // Initialize buffer
+        buffer = new List<byte>(); // Initialize buffer
         readPos = 0; // Set readPos to 0
         Write(_id); // Write packet id to the buffer
     }
@@ -39,7 +39,7 @@ public class Packet : IDisposable
     /// <param name="_data">The bytes to add to the packet.</param>
     public Packet(byte[] _data)
     {
-        buffer = []; // Initialize buffer
+        buffer = new List<byte>(); // Initialize buffer
         readPos = 0; // Set readPos to 0
         SetBytes(_data);
     }
